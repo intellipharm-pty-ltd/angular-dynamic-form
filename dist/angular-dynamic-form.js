d14e02fdcb508e7f27e73a601607b9653dec1501
Neil Russell <neil-intellipharm@github.com>
2015-03-20 09:08:08 +1000

    fixed submit on enter issue.

--- dist/angular-dynamic-form.js
+++ dist/angular-dynamic-form.js
@@ -9,12 +9,6 @@
     angular.module('AngularDynamicForm', [
         'angular.filter'
     ]);
-
-    /*angular.module('AngularDynamicForm').run(["$templateCache", function($templateCache) {
-        $templateCache.put("views/dynamic-form.html",
-            "<h1>DF</h1>"
-        );
-    }]);*/
 })();
 
 'use strict';
